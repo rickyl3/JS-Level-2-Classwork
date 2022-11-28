2a65f8fb0a0313a4ffc3e477bb3772b89068d1d1
rickyl3 <113804998+rickyl3@users.noreply.github.com>
2022-11-28 13:53:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+// function initialize() {
+//     halloweenText=document.getElementById("colordiv");
+
+//     fore = "black";
+//     back = "orange";
+
+//     textWidth = 100;
+// }
+// function toggleColor() {
+//     var tmp = fore;
+//     fore = back;
+//     back = tmp;
+
+//     display();
+// }
+// function display() {
+//     halloweenText.style.color = fore;
+//     halloweenText.style.backgroundColor = back;
+//     halloweenText.style.width = textWidth + "%";
+// }
+// function changeWidth(w) {
+//     textWidth = w;
+//     display();
+// }
+
+// function initialize() {
+//     textOutput = document.getElementById("textdiv");
+//     fontSizeOutput = document.getElementById("sizeout");
+//     fontSize = 24;
+// }
+// function modifyFontSize(mod) {
+//     fontSize += mod;
+//     if (fontSize < 4) {
+//         fontSize = 4;
+//     }
+//     display();
+// }
+// function display() {
+//     fontSizeOutput.innerHTML = fontSize;
+//     textOutput.style.fontSize = fontSize + "pt";
+// }
+
+function initialize() {
+    numberOutput = document.getElementById("numout");
+    rndNum = 0;
+
+    display();
+}
+function getRandomInteger(lower, upper) {
+    //R = parseInt(rnd * (upper - (lower - 1)) + lower)
+    var multiplier = upper - (lower - 1);
+    var rnd = parseInt(Math.random() * multiplier) + lower;
+
+    return rnd;
+}
+function changeNumber() {
+    rndNum = getRandomInteger(1, 10);
+    display();
+}
+function display() {
+    numberOutput.innerHTML = rndNum;
+}
